af6b52a5fa84b168f23a36eaa4c61219f002452e
naveen-kumar-v <naveengv75@gmail.com>
2023-06-26 17:09:25 +0530

    Added private route for '/dashboard'.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import React, { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 
 import NavBar from './components/NavBar';
+import PrivateRoute from './components/PrivateRoute'
 
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import ContactUs from './pages/ContactUs';
 
 
 
@@ -27,9 +30,15 @@ function App() {
 
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<ContactUs />} />
         <Route path='/signup' element={<SignUp setIsLoggedIn={setIsLoggedIn} />} />
         <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/dashboard' element={
+          <PrivateRoute isLoggedIn={isLoggedIn}>
+            <Dashboard />
+          </PrivateRoute>
+        } />
       </Routes>
     </div>
   );
